d527fd5bc5f05c6c01ccedd717ec473f5c71d16a
Brime-c <olabarriaga.felipe@gmail.com>
2026-06-15 00:16:51 -0300

    lesson 2-1

--- main.go
+++ main.go
@@ -8,12 +8,20 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.HandleFunc(http.ResponseWriter, *http.Request)
+	fileServer := http.FileServer(http.Dir("."))
+	prefixed := http.StripPrefix("/app", fileServer)
+	mux.Handle("/app/", prefixed)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
 	myServer := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
+
 	log.Fatal(myServer.ListenAndServe())
 
 }
